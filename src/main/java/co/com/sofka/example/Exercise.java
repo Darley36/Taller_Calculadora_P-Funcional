e426744a4bf7205cdeed4e209ddb8dba40e9c9ff
Darley36 <darleybest@gmail.com>
2021-10-04 23:44:03 -0500

    Sum, Subtract and Multiply added

--- src/main/java/co/com/sofka/example/Exercise.java
+++ src/main/java/co/com/sofka/example/Exercise.java
@@ -5,4 +5,21 @@ public class Exercise {
     public static int sumNumbers(int valueOne, int valueTwo){
         return (valueOne + valueTwo);
     }
+
+    public static int subtractNumbers(int valueOne, int valueTwo){
+        return (valueOne - valueTwo);
+    }
+
+    public static int multiplyNumbers(int valueOne, int valueTwo){
+        if (valueTwo > 0) {
+            return sumNumbers(valueOne, multiplyNumbers(valueOne,valueTwo-1));
+        }
+        return 0;
+    }
+
+    public static void main(String[] args){
+        System.out.println("La suma de 3 + 4 es: "+sumNumbers(3,4));
+        System.out.println("La resta de 8 - 6 es: "+subtractNumbers(8,6));
+        System.out.println("La multiplicacion de 6 * 6 es: "+multiplyNumbers(6,6));
+    }
 }
